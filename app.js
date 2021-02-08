d6ab0a3284dd508f7dfcf06113a67eec14412356
ngo275 <ngo38114@gmail.com>
2021-02-08 22:57:13 +0700

    Migrated credify-js to credify-nodejs

--- app.js
+++ app.js
@@ -1,38 +1,120 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { getUserInfo } = require('@credify/credify-js');
+const Credify = require("credify-nodejs");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const port = 8000;
 
-// TEST APP configuration
-const mnemonic = "crime cake room tiny mirror exercise idea provide exhaust staff olive raise";
-const config = {
-  apiKey: "bufIgxAyApnAVUdeZjyEfWXjbOhrEYTSBe3rGzqNbiDEJDtQBpALMumG2n92Q6lF",
-  mode: "sandbox" // If you are using Production, it will be `production`.
+
+///////////////////////////////////////////////////////
+/// Config for auth
+///////////////////////////////////////////////////////
+
+const signingPrivateKey = `-----BEGIN PRIVATE KEY-----
+MC4CAQAwBQYDK2VwBCIEIGSTUi4/MKS6laRoj5Cirazy2gkKpBu6c0I6c/TXUvQu
+-----END PRIVATE KEY-----`;
+const apiKey = "DNLYHGAjm1VW6kJykit5Jn0XNdI5lmQY5m6wAbgIU7h4L1jUIFRr4HqeOsmvLevW";
+
+
+///////////////////////////////////////////////////////
+/// Config for OIDC Client
+///////////////////////////////////////////////////////
+
+const organizationId = "37c5abfa-a4b7-4521-a312-89a2ec53e804";
+const redirectUrl = "http://localhost:3000/callback";
+const scopes = ["openid", "phone", "email", "profile"];
+
+
+///////////////////////////////////////////////////////
+/// Config for Claim Provider
+///////////////////////////////////////////////////////
+
+const claims = {
+  scopenamea: {
+    // NOTE: This needs to be actual user's data.
+    claimnamea: 123,
+  },
+  scopenameb: {
+    claimnameb: "test"
+  },
 };
 
-app.post('/', (req, res) => {
-  const accessToken = req.body.access_token;
-  // const accessToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6InB1YmxpYzo4ZDA1NjJmNS0zMjI0LTQ1OGYtODBkZS01YjczNTdkMWUwMjIiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOltdLCJjbGllbnRfaWQiOiJjMDNlYWVkZC0yYTIyLTQyYWQtYTYxZi1jNzkxZDY2YjMwYzgiLCJleHAiOjE1OTkyMDA1OTIsImV4dCI6e30sImlhdCI6MTU5ODU5NTc5MSwiaXNzIjoiaHR0cHM6Ly9zdGFnZS1vaWRjLWNvcmUuY3JlZGlmeS5vbmUvIiwianRpIjoiZDBkOGJlM2MtNmYyYS00MWJiLThjY2QtZDQ5YTk4ZDA2MTE3IiwibmJmIjoxNTk4NTk1NzkxLCJzY3AiOlsicHJvZmlsZSIsInBob25lIiwiZW1haWwiLCJvcGVuaWQiXSwic3ViIjoiYjg4ZTJjODItMzk3Yi00ZjQxLWE3NDYtZTFmNjU2MTIyNTYyIn0.fU9QBpQajLWONHCP5N8B9qE4sSI9_vWIiJB-1YOn3X57WUNmFQqwjgJPnWUieimUh43xcuVCIEH86fl_KXIRHusMJTOqZwXLXGUal71JHNZlR7v4mECA4CDBWXcZzN-4OwQeJ6OoNmnqAoYQWtE7V-VUbUfrj-XKSFdrNMM-AHytb9Ir3Le_-Kz4OpfkVa680DLo1UXcslF_WToygTzRcsJBc10T0-E9SNk6-LCnp37lEBp7JpnAWinXCfUWU7_IaUj6iFesAYmgLJc6kcC4EeyfDswuOBOfEklb4qVP8fRKBv22KeA9LNcZuC0oVb60gYq8OL20vvcZKZZ7H1uxHAKL1ZeAw5UI9KcCtsVazja_29JXiY2Io0Ge55ecBeiS-D5AfTwJxP56jcKQ-gZ-oAxGUrd8EFVpBf_yGf0z2WJHEEtcCeT68JPoHAnEl9Dv6n84eKxl54nt-1zp8kifni7fqktnChrnunCEZhXfxdtu6yrpRYTMb0YC-Jk90GgDnVkCtC0rr40BJDKqTX9pgjVG8UwLg9myjaobM9bEVLENA_RS_0Pt7VWAUmdgJUyVrnM2FsQuwYLq_Z90dCfI2aFEpFjuZTwH61bVDDzdR6vfV2k5i9hfbEtoNXW3wZ_MTgIwziSYP0M5V1NU-ziJhBfnjroFqGL55sSbYS-pP6M";
-  getUserInfo(mnemonic, accessToken, config).then((result) => {
-    console.log(result);
-    // sid is session ID.
-    console.log(`User ID: ${result.sub}`);
-    console.log(`Email: ${result.email}`);
-    console.log(`Phone Number: ${result.phoneNumber}`);
 
-    // Do your job
+/// Initialize Credify
+const credify = new Credify(signingPrivateKey, apiKey, { mode: "development" });
 
-    res.send(result);
-  }).catch((err) => {
-    res.send(err);
-  })
+
+///////////////////////////////////////////////////////
+/// Handlers for data receiving services
+///////////////////////////////////////////////////////
+
+app.get("/oidc", async (req, res) => {
+  const state = Math.random().toString();
+  const options = { state };
+  if (req.query.phone_number) {
+    options["phone_number"] = req.query.phone_number;
+  } else if (req.query.entity_id) {
+    options["entity_id"] = req.query.entity_id;
+  }
+  console.log(req);
+
+  const { oidcUrl, privateKey } = await credify.oidc.initiateOIDC(organizationId, redirectUrl, scopes, options);
+
+  // TODO: store this 'privateKey' along with 'state' to DB
+  console.log(state, privateKey);
+
+  res.send({ url: oidcUrl });
 });
 
+app.post("/oidc", async (req, res) => {
+  const accessToken = req.body.access_token;
+  const state = req.body.state;
+
+  // TODO: find the private key by state
+  const encryptionPrivateKey = "";
+
+  const data = await credify.oidc.userinfo(accessToken, encryptionPrivateKey);
+  console.log(data);
+
+  // NOTE: You can handle whatever you want. Offer redemption, as well.
+
+  res.send({ ...data });
+});
+
+
+///////////////////////////////////////////////////////
+/// Handlers for data providing services
+///////////////////////////////////////////////////////
+
+app.post("/create", async (req, res) => {
+  const profile = req.body.profile;
+  const password = req.body.password;
+  const id = await credify.entity.create(profile, password);
+
+  const commitments = await credify.claims.push(id, claims);
+
+  // TODO: store this 'id' and 'commitments' to DB
+  res.send({ id });
+});
+
+app.post("/user-counts", async (req, res) => {
+  res.send("coming soon.");
+});
+
+app.post("/offer-evaluate", async (req, res) => {
+  res.send("coming soon.");
+});
+
+app.post("/encrypted-claims", async (req, res) => {
+  res.send("coming soon.");
+});
+
+
+/// Start server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
+
